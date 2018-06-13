86dfb4945af91c58e283f9d22919c5cc5a32beeb
nullpo-no-kei <eninsr.think@gmail.com>
2018-06-14 01:36:53 +0900

    ・検索エンジンリストの動的読み込み. ・キーワード検索→ブラウザ連動. ・検索エンジン切り替えなど.

---
+++ SearchLogBrowser/SearchLogBrowser/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchLogBrowser
+{
+    class GenericKeyValue
+    {
+        public String Key { get; set; }
+        public String Value { get; set; }
+    }
+
+    class ComboBoxItem: GenericKeyValue
+    {
+        public override string ToString()
+        {
+            return Key;
+        }
+    }
+}
--- SearchLogBrowser/SearchLogBrowser/MainWindow.xaml.cs
+++ SearchLogBrowser/SearchLogBrowser/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Configuration;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace SearchLogBrowser
 {
@@ -25,6 +27,14 @@ namespace SearchLogBrowser
         {
             InitializeComponent();
             // 設定ファイルの読み込み.
+            List<ComboBoxItem> list
+                = JsonConvert.DeserializeObject<List<ComboBoxItem>>(ConfigurationManager.AppSettings["searchEngineList"]);
+            searchEngineList.Items.Clear();
+            foreach (ComboBoxItem item in list)
+            {
+                searchEngineList.Items.Add(item);
+            }
+            searchEngineList.SelectedIndex = 0;
             // browser.Navigate(new Uri(startUrl));
             browser.Address = startUrl;
             addressBar.Text = new Uri(startUrl).ToString();
@@ -55,6 +65,28 @@ namespace SearchLogBrowser
         private void Next_Click(object sender, RoutedEventArgs e)
         {
             if (browser.CanGoForward) browser.GetBrowser().GoForward();
+        }
+
+        /**
+         * 検索エンジン.選択イベント.
+         */
+        private void SearchEngineList_Change(object sender, SelectionChangedEventArgs e)
+        {
+            //browser.Address = new Uri(((ComboBoxItem) searchEngineList.SelectedItem).Value).ToString();
+        }
+
+        /**
+         * 検索ワード.キー押下イベント
+         */
+        private void SearchWord_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.Equals(Key.Enter))
+            {
+                // browser.Navigate(new Uri(addressBar.Text));
+                var searchUrl = new Uri(((ComboBoxItem)searchEngineList.SelectedItem).Value + searchWord.Text);
+                browser.Address = searchUrl.ToString();
+                addressBar.Text = searchUrl.ToString();
+            }
         }
     }
 }
